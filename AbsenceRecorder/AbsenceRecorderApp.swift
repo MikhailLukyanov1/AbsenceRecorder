7a1c8da0dccf6436a1ff23af33cb2c9c0e4bc50c
MikhailLukyanov1 <lukyanov.m@etoncollege.org.uk>
2023-01-29 16:00:02 +0000

    Initial Commit

---
+++ AbsenceRecorder/AbsenceRecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  AbsenceRecorderApp.swift
+//  AbsenceRecorder
+//
+//  Created by MIKHAEL LUKYANOV on 29/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct AbsenceRecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
