104af0fb4ad14c289e8214802c11f36924439c74
MikhailLukyanov1 <lukyanov.m@etoncollege.org.uk>
2023-01-29 17:15:34 +0000

    models

---
+++ AbsenceRecorder/Model/Division.swift
@@ -0,0 +1,19 @@
+//
+//  Division.swift
+//  AbsenceRecorder
+//
+//  Created by MIKHAEL LUKYANOV on 29/01/2023.
+//
+
+import Foundation
+
+
+
+class Division {
+    let code: String
+    var students: [Student] = []
+    
+    init(code: String) {
+        self.code = code
+    }
+}
